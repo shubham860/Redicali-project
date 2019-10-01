92333bf6a1f818a2fd66fd6c49e02095f2117f12
shubham860 <shubham.chauhan860@gmail.com>
2019-10-02 01:45:56 +0530

    update

---
+++ redicali/src/Actions/operations.js
@@ -0,0 +1,14 @@
+const Add_New  = 'Add_New'
+const Delete = 'Delete'
+
+export const addnew = () => {
+  return{
+    type : Add_New
+  }
+}
+
+export const Delete = () => {
+  return{
+    type : Delete
+  }
+}
